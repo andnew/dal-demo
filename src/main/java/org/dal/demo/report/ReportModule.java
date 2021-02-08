87bae6b329dcd1b2ba44c89fb2aecc022e1abda7
andrew <ywang3394@gmail.com>
2021-02-08 10:14:57 +0800

    添加 home 请求页

--- src/main/java/org/dal/demo/report/ReportModule.java
+++ src/main/java/org/dal/demo/report/ReportModule.java
@@ -6,7 +6,7 @@ import org.unidal.web.mvc.annotation.ModulePagesMeta;
 
 @ModuleMeta(name = "r", defaultInboundAction = "hello", defaultTransition = "default", defaultErrorAction = "default")
 @ModulePagesMeta({
-//        org.dal.demo.report.page.home.Handler.class,
+        org.dal.demo.report.page.home.Handler.class,
         org.dal.demo.report.page.hello.Handler.class
 })
 public class ReportModule extends AbstractModule {
